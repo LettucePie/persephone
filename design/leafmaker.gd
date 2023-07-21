061387c13432bb8a1258f649c1459e9f60fbf4bc
Matthew LettucePie Leemhuis <thelettucepie@gmail.com>
2023-07-21 09:47:55 -0400

    LeafPoint class system, probably switching to Curve2D

--- design/leafmaker.gd
+++ design/leafmaker.gd
@@ -5,10 +5,16 @@ export(Texture) var circle_node_tex
 export(Vector2) var node_scale = Vector2(0.1, 0.1)
 
 
-var nodes = []
+onready var leaf_poly = $Polygon2D
+var leaf_points = []
 
 
-# Called when the node enters the scene tree for the first time.
+class LeafPoint: 
+	var mode : bool
+	var node_vis : Sprite
+	var pos : Vector2
+
+
 func _ready():
 	pass # Replace with function body.
 
@@ -24,13 +30,25 @@ func _input(event):
 			print(event.button_index)
 			if event.button_index == 1:
 				print("Left Click")
-				var new_node = Sprite.new()
-				new_node.texture = square_node_tex
-				new_node.position = event.position
-				new_node.scale = node_scale
-				nodes.append(new_node)
-				self.add_child(new_node)
+				add_point(event.position)
 			if event.button_index == 2:
 				print("Right Click")
 			if event.button_index == 3:
 				print("Middle Click")
+
+
+func add_point(pos : Vector2):
+	var new_point = LeafPoint.new()
+	new_point.mode = false
+	var polygon = leaf_poly.get_polygon()
+	polygon.append(pos)
+	leaf_poly.set_polygon(polygon)
+	new_point.pos = pos
+	var new_node = Sprite.new()
+	new_node.texture = square_node_tex
+	new_node.position = pos
+	new_node.scale = node_scale
+	self.add_child(new_node)
+	new_point.node_vis = new_node
+	leaf_points.append(new_point)
+	
