6dcd42d5501b9334c79426ec1020d7cd301d3ff2
Matthew <thelettucepie@gmail.com>
2023-07-25 17:53:27 -0400

    Added starting Leaf to operate on

--- design/leafmaker_canvas.gd
+++ design/leafmaker_canvas.gd
@@ -4,36 +4,39 @@ export(Texture) var square_node_tex
 export(Texture) var circle_node_tex
 export(Vector2) var node_scale = Vector2(0.1, 0.1)
 
+enum Mode {ADD_MODE, EDIT_MODE, DELETE_MODE}
 export(bool) var debug_draw = false
 
 
 onready var leaf_poly = $Polygon2D
 onready var leaf_origin = $LeafOrigin
-var leaf_points = []
 var leaf_curve : Curve2D
 
-
-class LeafPoint: 
-	var mode : bool
-	var node_vis : Sprite
-	var pos : Vector2
-	var curve_index : int
-	
-	func _init(set_mode : bool, set_vis : Sprite, set_pos : Vector2, set_idx : int):
-		mode = set_mode
-		node_vis = set_vis
-		pos = set_pos
-		curve_index = set_idx
-
+var current_mode = Mode.ADD_MODE
 
 func _ready():
 	leaf_curve = Curve2D.new()
-	leaf_curve.set_bake_interval(50)
+	leaf_curve.set_bake_interval(5)
 	leaf_curve.add_point(leaf_origin.position, Vector2.ZERO, Vector2.ZERO)
+	_starting_leaf()
 
 
 func _input(event):
 	pass
+
+
+func _starting_leaf():
+	var top_point = leaf_origin.position
+	top_point.y = 50
+	var center_point = leaf_origin.position.linear_interpolate(top_point, 0.3)
+	var left_point = center_point
+	left_point.x = left_point.x * 0.7
+	var right_point = center_point
+	right_point.x = OS.get_window_size().x - left_point.x
+	leaf_curve.add_point(left_point, Vector2.ZERO, Vector2.ZERO)
+	leaf_curve.add_point(top_point, Vector2.ZERO, Vector2.ZERO)
+	leaf_curve.add_point(right_point, Vector2.ZERO, Vector2.ZERO)
+	update()
 
 
 func _on_table_gui_input(event):
@@ -43,7 +46,8 @@ func _on_table_gui_input(event):
 			print(event.button_index)
 			if event.button_index == 1:
 				print("Left Click")
-				add_point(event.position)
+				if current_mode is Mode.ADD_MODE:
+					add_point(event.position)
 			if event.button_index == 2:
 				print("Right Click")
 			if event.button_index == 3:
@@ -58,7 +62,6 @@ func add_point(pos : Vector2):
 	node_vis.scale = node_scale
 	self.add_child(node_vis)
 	var index = leaf_curve.get_baked_points().find(pos)
-	leaf_points.append(LeafPoint)
 #	draw_shape()
 #	draw_curve_shape()
 	update()
@@ -99,3 +102,8 @@ func debug_draw_curve_shape():
 func _draw():
 	if leaf_curve.get_baked_points().size() > 3:
 		draw_polygon(leaf_curve.get_baked_points(), PoolColorArray())
+
+
+func _on_Mode_item_selected(index):
+	current_mode = index
+	print("Current Mode: ", current_mode)
