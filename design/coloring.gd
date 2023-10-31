99c1f8dfdd1ba33cc930797aed0e6e932a864ab9
Matthew LettucePie <thelettucepie@gmail.com>
2023-10-31 16:32:09 -0400

    Centered brush tip

--- design/coloring.gd
+++ design/coloring.gd
@@ -5,10 +5,11 @@ extends Control
 
 var canvas_img : Image
 var brush_tip : Image
+var brush_size : int = 24
+var brush_scale : float = 1.0
 
 var brush_pressed : bool = false
-var brush_stroke : Array
-var stroke_progress : int
+var prev_point : Vector2
 
 
 func _ready():
@@ -16,10 +17,11 @@ func _ready():
 	make_brush_tips()
 	canvas_img = Image.create(image_w, image_h, false, 5)
 	display_image()
+	print(brush_size, " ", brush_size * brush_size)
 
 
 func make_brush_tips():
-	var new_tip = Image.create(24, 24, false, 5)
+	var new_tip = Image.create(brush_size, brush_size, false, 5)
 	new_tip.fill(Color.WHITE)
 	brush_tip = new_tip
 
@@ -29,23 +31,7 @@ func display_image():
 
 
 func _process(delta):
-	if stroke_progress < brush_stroke.size() - 2:
-		var current = brush_stroke[stroke_progress]
-		var next = brush_stroke[stroke_progress + 1]
-		var dist = current.distance_squared_to(next)
-		var scry = dist > 10
-		var steps = int(dist / 2)
-		if stroke_progress + 2 < brush_stroke.size() and scry:
-			## Scry forward for curve
-			var ahead = brush_stroke[stroke_progress + 2]
-			var angle = current.direction_to(ahead).angle_to(current.direction_to(next))
-			var anchor = current + (current.direction_to(ahead).rotated(angle) * ((ahead - current).length() * 0.5))
-			for i in range(1, dist):
-				color_at(current.bezier_interpolate(anchor, anchor, next, float(i) / float(steps)))
-			stroke_progress += 1
-		elif !scry:
-			color_at(current)
-			stroke_progress += 1
+	pass
 
 
 func _input(event):
@@ -53,11 +39,9 @@ func _input(event):
 		if event.button_index == 1:
 			brush_pressed = event.pressed
 			if event.pressed:
-				brush_stroke.clear()
-				stroke_progress = 0
+				prev_point = convert_to_image_space($canvas.get_local_mouse_position(), $canvas.size)
 	if event is InputEventMouseMotion and brush_pressed:
-		brush_stroke.append(convert_to_image_space($canvas.get_local_mouse_position(), $canvas.size))
-#		color_at(convert_to_image_space($canvas.get_local_mouse_position(), $canvas.size))
+		color_at(convert_to_image_space($canvas.get_local_mouse_position(), $canvas.size))
 
 
 func _on_canvas_mouse_exited():
@@ -65,14 +49,20 @@ func _on_canvas_mouse_exited():
 
 
 func convert_to_image_space(canvas_point : Vector2, dimension : Vector2):
-	print("Canvas Point: ", canvas_point, " Dimension: ", dimension)
+#	print("Canvas Point: ", canvas_point, " Dimension: ", dimension)
 	var x = canvas_point.x / dimension.x
 	var y = canvas_point.y / dimension.y
 	return Vector2(int(image_w * x), int(image_h * y))
 
 
 func color_at(point : Vector2):
-	canvas_img.blend_rect(brush_tip, Rect2i(Vector2i.ZERO, Vector2i(24, 24)), point)
+	print(point.distance_squared_to(prev_point))
+	prev_point = point
+	var center_offset = point - (Vector2(brush_size, brush_size) * 0.5)
+	canvas_img.blend_rect(
+		brush_tip, 
+		Rect2i(Vector2i.ZERO, Vector2i(brush_size, brush_size)), 
+		center_offset)
 	display_image()
 
 
