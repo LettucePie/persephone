3b559ad8f21931529376acf0b646521b1f747e15
Matthew LettucePie <thelettucepie@gmail.com>
2023-11-16 17:09:24 -0500

    setting up solution for color picker scale

--- design/ui/custom_color_picker.gd
+++ design/ui/custom_color_picker.gd
@@ -2,6 +2,9 @@ extends ColorPickerButton
 
 func _ready():
 	connect("picker_created", simplify_colorpicker)
+	connect("resized", color_picker_scale)
+	simplify_colorpicker()
+	color_picker_scale()
 
 func simplify_colorpicker():
 #	if OS.has_feature("mobile"):
@@ -13,3 +16,12 @@ func simplify_colorpicker():
 	color_picker.set_presets_visible(false)
 	color_picker.set_sampler_visible(false)
 	color_picker.set_sliders_visible(false)
+
+
+func color_picker_scale():
+	print("Color Picker Scale")
+	var new_scale = Vector2(0.8, 0.8).lerp(
+	Vector2(1.8, 1.8), 
+	inverse_lerp(1000, 4500, get_window().get_size().length())
+	)
+	get_picker().scale = new_scale
