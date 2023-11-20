65d0ab2301535ba9ee0bd15bd67307fd41b67c30
Matthew LettucePie <thelettucepie@gmail.com>
2023-11-20 12:58:38 -0500

    cleanup working on color picker scale

--- design/ui/custom_color_picker.gd
+++ design/ui/custom_color_picker.gd
@@ -4,11 +4,9 @@ extends ColorPickerButton
 
 func _ready():
 	connect("picker_created", simplify_colorpicker)
-#	connect("resized", picker_window_scale)
 	get_picker().get_parent().connect("visibility_changed", picker_window_scale)
 	simplify_colorpicker()
 	manipulate_color_picker_internals(get_picker())
-#	picker_window_scale()
 
 
 func simplify_colorpicker():
@@ -21,8 +19,6 @@ func simplify_colorpicker():
 	color_picker.set_presets_visible(false)
 	color_picker.set_sampler_visible(false)
 	color_picker.set_sliders_visible(false)
-#	manipulate_color_picker_internals(color_picker)
-#	picker_window_scale()
 
 
 func update_children_size_flags(children : Array):
@@ -45,11 +41,8 @@ func manipulate_color_picker_internals(picker : ColorPicker):
 	print("Starting Manipulations of ColorPicker: ", picker)
 	get_picker().size_flags_horizontal = 3
 	get_picker().size_flags_vertical = 3
-#	var scope_goal = picker.get_child_count(true)
-#	var current_scope = 0
 	var backup_breaker = 10
 	var layer = picker.get_children(true)
-#	var layer = picker.get_children(true)[0].get_children(true)
 	while(layer.size() > 0 or backup_breaker > 0):
 		backup_breaker -= 1
 		layer = update_children_size_flags(layer)
@@ -72,31 +65,5 @@ func picker_window_scale():
 		new_pos.x = screen_center.x - (new_scale.x / 2)
 	var win : Window = get_picker().get_parent()
 	win.wrap_controls = false
-#	win.content_scale_mode = 1
-#	win.mode = 3
-#	win.set_content_scale_factor(1.2)
 	win.position = new_pos
-#	win.size = new_scale
 	win.min_size = new_scale
-#	print("H_Flags : ", get_picker().size_flags_horizontal)
-#	print("V_Flags : ", get_picker().size_flags_vertical)
-#	panel["theme_override_styles/panel"] = stylebox
-	var deep_children : Array = get_picker().get_children(true)
-	for deep_child in deep_children:
-		deep_child.size_flags_horizontal = 3
-		deep_child.size_flags_vertical = 3
-#	get_picker().pivot_offset = get_picker().size
-#	var old_size = get_picker().size
-#	var new_scale = Vector2(0.8, 0.8).lerp(
-#	Vector2(2.0, 2.0), 
-#	inverse_lerp(1000, 4500, get_window().get_size().length())
-#	)
-#	print("Color Picker Rect Before: ", get_picker().get_rect())
-#	get_picker().scale = new_scale
-#	get_picker().position += old_size - get_picker().size
-#	get_picker().size = Vector2(1100, 1000)
-#	get_picker().position = Vector2(-1000, -1000)
-#	get_picker().custom_minimum_size = Vector2(800, 600)
-#	print("Color Picker Scale : ", new_scale)
-#	print("Color Picker Rect After: ", get_picker().get_rect())
-
