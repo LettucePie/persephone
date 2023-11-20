e2cdefed8004bd8dd6729b68438b06e262021d95
Matthew LettucePie <thelettucepie@gmail.com>
2023-11-20 11:00:32 -0500

    made small progress on scaling up colorpicker

--- design/ui/custom_color_picker.gd
+++ design/ui/custom_color_picker.gd
@@ -1,10 +1,12 @@
 extends ColorPickerButton
 
+@export var stylebox : StyleBox
+
 func _ready():
 	connect("picker_created", simplify_colorpicker)
 	connect("resized", color_picker_scale)
 	simplify_colorpicker()
-	color_picker_scale()
+#	color_picker_scale()
 
 func simplify_colorpicker():
 #	if OS.has_feature("mobile"):
@@ -16,12 +18,18 @@ func simplify_colorpicker():
 	color_picker.set_presets_visible(false)
 	color_picker.set_sampler_visible(false)
 	color_picker.set_sliders_visible(false)
+	color_picker_scale()
 
 
 func color_picker_scale():
-	print("Color Picker Scale")
+#	panel["theme_override_styles/panel"] = stylebox
 	var new_scale = Vector2(0.8, 0.8).lerp(
 	Vector2(1.8, 1.8), 
 	inverse_lerp(1000, 4500, get_window().get_size().length())
 	)
-	get_picker().scale = new_scale
+	print("Color Picker Rect Before: ", get_picker().get_rect())
+#	get_picker().scale = new_scale
+#	get_picker().position = Vector2(-1000, -1000)
+	get_picker().custom_minimum_size = Vector2(1000, 400)
+#	print("Color Picker Scale : ", new_scale)
+	print("Color Picker Rect After: ", get_picker().get_rect())
