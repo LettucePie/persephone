7bbac4131852794471db84dbbc7fd3a78f9b7770
Matthew LettucePie <thelettucepie@gmail.com>
2023-11-20 12:56:18 -0500

    FINALLY fixed color picker scale

--- design/ui/custom_color_picker.gd
+++ design/ui/custom_color_picker.gd
@@ -7,7 +7,7 @@ func _ready():
 #	connect("resized", picker_window_scale)
 	get_picker().get_parent().connect("visibility_changed", picker_window_scale)
 	simplify_colorpicker()
-#	manipulate_color_picker_internals(get_picker())
+	manipulate_color_picker_internals(get_picker())
 #	picker_window_scale()
 
 
@@ -21,6 +21,7 @@ func simplify_colorpicker():
 	color_picker.set_presets_visible(false)
 	color_picker.set_sampler_visible(false)
 	color_picker.set_sliders_visible(false)
+#	manipulate_color_picker_internals(color_picker)
 #	picker_window_scale()
 
 
@@ -32,6 +33,8 @@ func update_children_size_flags(children : Array):
 			print(child)
 			child.size_flags_horizontal = 3
 			child.size_flags_vertical = 3
+			if child.custom_minimum_size != Vector2.ZERO:
+				child.custom_minimum_size = Vector2(400, 400)
 			if child.get_child_count(true) > 0:
 				populated_children += child.get_children(true)
 	print("Returning ", populated_children.size(), " populated children")
@@ -46,6 +49,7 @@ func manipulate_color_picker_internals(picker : ColorPicker):
 #	var current_scope = 0
 	var backup_breaker = 10
 	var layer = picker.get_children(true)
+#	var layer = picker.get_children(true)[0].get_children(true)
 	while(layer.size() > 0 or backup_breaker > 0):
 		backup_breaker -= 1
 		layer = update_children_size_flags(layer)
@@ -55,11 +59,17 @@ func manipulate_color_picker_internals(picker : ColorPicker):
 
 func picker_window_scale():
 	var screen_size : Vector2i = get_window().get_size()
-	var new_scale = Vector2i(screen_size.x - 10, screen_size.x - 10)
+	var screen_center = screen_size / 2
+	var new_scale = Vector2i(
+		screen_size.x - 10, 
+		clamp(screen_size.x - 10, 256, 424))
+	var new_pos = Vector2i(5, screen_center.y - (new_scale.y / 2))
 	if screen_size.x > screen_size.y:
 		## scale by height
-		new_scale = Vector2i(screen_size.y - 10, screen_size.y - 10)
-	var new_pos = Vector2i(5, new_scale.y / 3)
+		new_scale = Vector2i(
+			clamp(screen_size.y - 10, 256, 424), 
+			clamp(screen_size.y - 10, 256, 424))
+		new_pos.x = screen_center.x - (new_scale.x / 2)
 	var win : Window = get_picker().get_parent()
 	win.wrap_controls = false
 #	win.content_scale_mode = 1
