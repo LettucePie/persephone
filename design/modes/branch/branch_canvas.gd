fd5f5c7b1e7badc050e45959ec0ee50520ad18ca
Matthew LettucePie <thelettucepie@gmail.com>
2023-11-29 17:27:21 -0500

    starting setup of branch width curve manipulation

--- design/modes/branch/branch_canvas.gd
+++ design/modes/branch/branch_canvas.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var default_curve : Curve
+
 class Branch:
 	## Branch Shape Properties
 	var line : Line2D = null
@@ -14,10 +16,32 @@ class Branch:
 	var force_symmetry : bool = false
 	var leaf_size : float = 0.0
 
+var branches : Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	branches.clear()
+	var center = Vector2(get_window().get_size() / 2)
+	print(center)
+	var new_line : Line2D = Line2D.new()
+	new_line.add_point(center)
+	new_line.add_point(center + (Vector2.UP * 200))
+	add_child(new_line)
+	var new_branch : Branch = Branch.new()
+	new_branch.line = new_line
+	branches.append(new_branch)
+	set_branch_width_curve(new_branch, 20, 10)
+
+
+func set_branch_width_curve(branch : Branch, start_w : float, end_w : float):
+	var line : Line2D = branch.line
+	line.width = start_w
+	var lowest_percent : float = end_w / start_w
+	var new_curve : Curve = default_curve.duplicate()
+	new_curve.set_point_value(1, lowest_percent)
+#	new_curve.min_value = end_w / start_w
+	new_curve.bake()
+	line.set_curve(new_curve)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
