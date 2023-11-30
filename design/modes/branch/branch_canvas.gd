8deaad1eb670986147b21749d9dc64b1bd19ca66
Matthew LettucePie <thelettucepie@gmail.com>
2023-11-30 12:23:09 -0500

    maybe figured out a solid loop for populating layers of branches

--- design/modes/branch/branch_canvas.gd
+++ design/modes/branch/branch_canvas.gd
@@ -5,30 +5,42 @@ extends Node2D
 
 class BranchLayer:
 	var layer : int = 0
-	var master_branch_copy : Branch
+	var master_branch_copy : Branch = null
 	var population : float = 0.0
-	var length_random : float = 0.0
+	var population_min : int = 1
+	var population_max : int = 1
 	var branches : Array = []
+	var growth_points : Array = []
 
 
 class Branch:
-	## Branch Type Properties
-	var layer : int = 0
-	## Branch Shape Properties
 	var line : Line2D = null
+	## Branch Shape Properties
 	var start_point : Vector2 = Vector2.ZERO
 	var start_normal : Vector2 = Vector2.RIGHT
 #	var target_points : PackedVector2Array = [Vector2.UP]
 	var length : float = 1.0
+	var length_random : float = 0.0
 	var jagginess : float = 0.0
+	var upward_direction : Vector2 = Vector2.UP
 	var upward_angle : float = 0.0
 	var upward_influence : float = 0.0
+	var start_width : float = 20.0
+	var end_width : float = 1.0
 	## Growth Point Properties
 	var carries_leaves : bool = false
 	var population : float = 0.0
 	var growth_coverage : float = 0.0
+	var growth_coverage_end_to_base : bool = true
 	var force_symmetry : bool = false
 	var leaf_size : float = 0.0
+	var leaf_size_random : float = 0.0
+
+
+class GrowthPoint:
+	var point_position : Vector2 = Vector2.ZERO
+	var point_width : float = 0.0
+
 
 var branch_layers : Array
 #var branches : Array
@@ -60,8 +72,7 @@ func make_trunk():
 	trunk.start_point.y *= 0.7
 	trunk.start_normal = Vector2.UP
 	trunk.length = trunk.start_point.y - (screen_size.y * 0.2)
-#	trunk.target_points.append(Vector2(screen_size.x / 2, screen_size.y * 0.3))
-	make_layer(0, trunk)
+	make_layer(0, setup_branch_line(trunk))
 
 
 func make_layer(layer_num : int, master_branch : Branch):
@@ -72,21 +83,56 @@ func make_layer(layer_num : int, master_branch : Branch):
 		var new_layer : BranchLayer = BranchLayer.new()
 		new_layer.layer = layer_num
 		new_layer.master_branch_copy = master_branch
+		var previous_layer_growth_points = []
+		if layer_num == 0:
+			## Trunk Layer
+			new_layer.population_min = 1
+			new_layer.population_max = 1
+			new_layer.population = 1.0
+		else:
+			previous_layer_growth_points = branch_layers.back().growth_points
+			new_layer.population_min = 2 * layer_num
+			new_layer.population_max = 18 * layer_num
+			new_layer.population = 1.0
+		populate_layer(new_layer, previous_layer_growth_points)
+		map_out_growth_points(new_layer)
 		branch_layers.append(new_layer)
+		draw_all_branches()
 
 
-func set_branch_width_curve(branch : Branch, start_w : float, end_w : float):
-	var line : Line2D = branch.line
-	line.width = start_w
-	var lowest_percent : float = end_w / start_w
-	var new_curve : Curve = default_curve.duplicate()
-	new_curve.set_point_value(1, lowest_percent)
-#	new_curve.min_value = end_w / start_w
-	new_curve.bake()
-	line.set_curve(new_curve)
+func populate_layer(layer : BranchLayer, start_points : Array):
+	print("Iterate over Start Points, making a new branch at each")
 
 
-func create_branch_line(branch : Branch):
+func map_out_growth_points(layer : BranchLayer):
+	print("Iterate over fresh branches, gathering all viable growth points")
+
+
+func draw_all_branches():
+	for b_l in branch_layers:
+		if b_l.branches.size() == 0:
+			print("Fresh Layer, has no branches")
+			print("Initialize Branches")
+			if b_l.layer <= 0:
+				print("Drawing Trunk")
+			
+			## Okay ... so
+			## if the branch coverage area / population changes
+			## then we need to change the number of branches in Branch Layer
+			## if the population percent in the Branch Layer changes
+			## then we need to change the number of branches in Branch Layer
+			## weird...
+			
+			## But also we shouldn't be remaking every branch every time, so...
+			## we should ask if these values are different, or worth checking first.
+			## then adjust all the branches in the branch_layer
+			
+			## if the branch layer is empty in the first place, we need a
+			## check for that, and an initial populating function.
+			
+
+
+func setup_branch_line(branch : Branch):
 	if branch.line != null:
 		branch.line.clear_points()
 	else:
@@ -104,33 +150,32 @@ func create_branch_line(branch : Branch):
 		var segment_percent = float(p + 1) / 3.0
 		var influence = influence_curve.sample(segment_percent) \
 		* branch.upward_influence
-		direction = direction.slerp(Vector2.UP, influence)
+		direction = direction.slerp(branch.upward_direction, influence)
 		branch_points.append(reference + (direction * segment_length))
-	## For Jagginess:
-	## Find direction from Start to last branch_points[]
-	## Calc the 90 degree positive and negative of that angle, called up and down
-	## re-iterate over the branch_points[] and add up and down direction, 
-	## multiplied by jagginess value.
+	var sway_direction = \
+		branch.start_point.direction_to(branch_points[2]).rotated(PI / 2)
+	var flip_direction = 1
+	for bp in branch_points:
+		bp += (sway_direction * flip_direction) * branch.jagginess
+		if flip_direction > 0:
+			flip_direction = -1
+		else:
+			flip_direction = 1
+		branch.line.add_point(bp)
+	set_branch_width_curve(branch)
+	return branch
 
 
-func draw_all_branches():
-	for b_l in branch_layers:
-		if b_l.layer <= 0:
-			print("Drawing Trunk")
-			## Okay ... so
-			## if the branch coverage area / population changes
-			## then we need to change the number of branches in Branch Layer
-			## if the population percent in the Branch Layer changes
-			## then we need to change the number of branches in Branch Layer
-			## weird...
-			
-			## But also we shouldn't be remaking every branch every time, so...
-			## we should ask if these values are different, or worth checking first.
-			## then adjust all the branches in the branch_layer
-			
-			## if the branch layer is empty in the first place, we need a
-			## check for that, and an initial populating function.
-			
+func set_branch_width_curve(branch : Branch):
+	var line : Line2D = branch.line
+	line.width = branch.start_width
+	var lowest_percent : float = branch.end_width / branch.start_width
+	var new_curve : Curve = default_curve.duplicate()
+	new_curve.set_point_value(1, lowest_percent)
+	new_curve.bake()
+	line.set_curve(new_curve)
+
+
 
 
 func _process(delta):
--- design/modes/branch/branch_designer.gd
+++ design/modes/branch/branch_designer.gd
@@ -3,6 +3,14 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var dir = Vector2.UP
+	print(dir)
+	dir = dir.rotated(PI / -2)
+	print(dir)
+#	print(rad_to_deg(PI / 2))
+#	print(dir.from_angle(PI / 2))
+#	print(rad_to_deg(PI / 4))
+#	print(dir.from_angle(PI / 4))
 	pass # Replace with function body.
 
 
